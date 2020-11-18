8bc77d7e01183aa21e9959cb48e5f6a80868b972
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2020-11-18 08:30:57 +0800

    ZhangGJ:Inner class

---
+++ java-basic/src/main/java/inner/Parcel6.java
@@ -0,0 +1,36 @@
+package inner;
+
+/**
+ * @Author ZhangGJ
+ * @Date 2020/11/18 07:53
+ */
+public class Parcel6 {
+    private void internalTracking(boolean b) {
+        if (b) {
+            class TrackingSlip {
+                private String id;
+
+                TrackingSlip(String s) {
+                    id = s;
+                }
+
+                String getSlip() {
+                    return id;
+                }
+            }
+            TrackingSlip ts = new TrackingSlip("slip");
+            String s = ts.getSlip();
+        }
+        // Can’t use it here! Out of scope:
+        //! TrackingSlip ts = new TrackingSlip("x");
+    }
+
+    public void track() {
+        internalTracking(true);
+    }
+
+    public static void main(String[] args) {
+        Parcel6 p = new Parcel6();
+        p.track();
+    }
+}
