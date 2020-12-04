43c2bbbef7c596e87a6c8da22ea2fa30b6c1b3f9
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2020-12-04 08:31:06 +0800

    ZhangGJ:File

---
+++ java-basic/src/main/java/io/DirList.java
@@ -0,0 +1,42 @@
+package io;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+class DirFilter implements FilenameFilter {
+    private Pattern pattern;
+
+    public DirFilter(String regex) {
+        pattern = Pattern.compile(regex);
+    }
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return pattern.matcher(name).matches();
+    }
+}
+
+/**
+ * @Author ZhangGJ
+ * @Date 2020/12/04 07:51
+ */
+public class DirList {
+    public static void main(String[] args) {
+        /**
+         * "."代表的路径：当前目录，项目所处的目录
+         */
+        File path = new File(".");
+        String[] list;
+        if (args.length == 0) {
+            list = path.list();
+        } else {
+            list = path.list(new DirFilter(args[0]));
+        }
+        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for (String dirItem : list) {
+            System.out.println(dirItem);
+        }
+    }
+}
