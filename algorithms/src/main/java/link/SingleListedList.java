7b2386ec17632bfb3d7315c5fe3b5f8f9be2c230
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2021-01-10 08:24:30 +0800

    Gavin:Linked list

--- algorithms/src/main/java/link/SingleListedList.java
+++ algorithms/src/main/java/link/SingleListedList.java
@@ -2,22 +2,31 @@ package link;
 
 /**
  * 单向链表
+ * <p>
+ * 链表是一种递归的数据结构，它或者为空（null)，或者是指向一个结点(node)的引用，该结点含有一个泛型的元素和一个指向另一条链表的引用
  *
  * @Author ZhangGJ
  * @Date 2020/12/30 07:03
  */
 public class SingleListedList<E> {
 
-    private Node first;
+    private Node<E> first;
 
-    private Node last;
+    private Node<E> last;
 
 
     /**
      * 结点
+     * LinkedList中的Node是嵌套类
+     * <p>
+     * 如果不需要内部类对象与其外围类对象之间的联系，那么可以将内部类声明为static，这通常称为嵌套类。
+     * 普通的内部类对象隐式的保存了一个引用，指向创建它的外围类对象，然而，当内部类是static时，就不是这样了，嵌套类意味着：
+     * 1）要创建嵌套类的对象，并不需要其外围类的对象；
+     * 2）不能从嵌套类的对象中访问非静态的外围类对象。
+     * 嵌套类与普通的内部类还有一个区别，普通内部类的字段与方法，只能放在类的外部层次上，所以普通内部类不能有static数据和static字段，也不能包含嵌套类，但是嵌套类可以包含所有这些东西。
      */
     private static class Node<E> {
-        Node next;
+        Node<E> next;
         E item;
 
         public Node(E item) {
@@ -31,9 +40,8 @@ public class SingleListedList<E> {
      * @param e
      */
     public void addFirst(E e) {
-        Node oldFirst = first;
-        Node<E> node = new Node<>(e);
-        first = node;
+        Node<E> oldFirst = first;
+        first = new Node<>(e);
         first.next = oldFirst;
         first.item = e;
     }
@@ -53,9 +61,8 @@ public class SingleListedList<E> {
      * @param e
      */
     public void addLast(E e) {
-        Node oldLast = last;
-        Node<E> node = new Node<>(e);
-        last = node;
+        Node<E> oldLast = last;
+        last = new Node<>(e);
         last.item = e;
         oldLast.next = last;
     }
@@ -68,7 +75,7 @@ public class SingleListedList<E> {
      * @param e
      */
     public void deleteLast(E e) {
-        for (Node x = first; x != null; x = x.next) {
+        for (Node<E> x = first; x != null; x = x.next) {
             if (x.next.next == null) {
                 x.next = null;
             }
