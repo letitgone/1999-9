e30d18ccb376d16565939157dd14ec7da145b74f
Gavin <48301286+letitgone@users.noreply.github.com>
2021-01-27 09:57:45 +0800

    Gavin:Test code

--- algorithms/src/main/java/sort/insertion/Test.java
+++ algorithms/src/main/java/sort/insertion/Test.java
@@ -10,12 +10,13 @@ import java.util.Random;
 public class Test {
 
     public static int[] sort2(int[] arr) {
-        for (int j = 1; j < arr.length; j++) {
+        int n = arr.length;
+        for (int j = 1; j < n; j++) {
             int key = arr[j];
             int i = j - 1;
             while (i >= 0 && arr[i] > key) {
                 arr[i + 1] = arr[i];
-                --i;
+                i--;
             }
             arr[i + 1] = key;
         }
